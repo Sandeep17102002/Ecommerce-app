422f247f32fdcfedaa2ead9775f44b0150b91ab0
Sandeep Reddy <119499504+Sandeep17102002@users.noreply.github.com>
2023-08-06 07:43:25 +0530

    Updated

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from "url"
 
 //configure env
 dotenv.config();
 
+// esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirnname=path.dirname(__filename);
 //databse config
 connectDB();
 
